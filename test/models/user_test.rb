3817fa4d920f3fe928de5b9695d53e885a299fdb
Tommy Pham <phamtommylong1991@gmail.com>
2018-04-03 10:59:33 -0700

    annotated user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :integer          not null, primary key
+#  username            :string           not null
+#  password_digest     :string           not null
+#  session_token       :string           not null
+#  email               :string           not null
+#  profile_picture_url :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
