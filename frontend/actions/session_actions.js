af51433f0ac497c0ea91243f23e7243cab7894a2
Tommy Pham <phamtommylong1991@gmail.com>
2018-04-03 13:13:59 -0700

    Completed session actions

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,19 +1,32 @@
 import * as SessionApiUtil from '../util/session_api_util';
 
-export const signup = (user) => (dispatch) => SessionApiUtil.signup(user)
-  .then(
-    (user) => dispatch(receiveCurrentUser(user)),
-    (errors) => dispatch(receiveSessionErrors(errors))
-  );
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 export const signup = (user) => (dispatch) => SessionApiUtil.signup(user)
   .then(
     (user) => dispatch(receiveCurrentUser(user)),
-    (errors) => dispatch(receiveSessionErrors(errors))
+    (errors) => dispatch(receiveSessionErrors(errors.responseJSON))
   );
 
-export const signup = (user) => (dispatch) => SessionApiUtil.signup(user)
+export const login = (user) => (dispatch) => SessionApiUtil.login(user)
   .then(
     (user) => dispatch(receiveCurrentUser(user)),
-    (errors) => dispatch(receiveSessionErrors(errors))
+    (errors) => dispatch(receiveSessionErrors(errors.responseJSON))
   );
+
+export const logout = () => (dispatch) => SessionApiUtil.logout()
+  .then(
+    () => dispatch(receiveCurrentUser(null)),
+    (errors) => dispatch(receiveSessionErrors(errors.responseJSON))
+  );
+
+const receiveCurrentUser = (currentUser) => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser
+});
+
+const receiveSessionErrors = (errors) => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+})
