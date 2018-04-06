08c74dabad92038c765113c62a098d80576e9337
Tommy Pham <phamtommylong1991@gmail.com>
2018-04-05 17:54:14 -0700

    Removed some unnecessary code for dragging

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -30895,36 +30895,28 @@ var BlogPostCreationForm = function (_React$Component) {
     }
 
     // https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_draggable
+    // Messed around with it but credit goes to the site above
 
   }, {
     key: 'dragElement',
     value: function dragElement(elmnt) {
-      console.log(elmnt);
       var pos1 = 0,
           pos2 = 0,
           pos3 = 0,
           pos4 = 0;
-      if (document.getElementById(elmnt.id + "header")) {
-        /* if present, the header is where you move the DIV from:*/
-        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-      } else {
-        /* otherwise, move the DIV from anywhere inside the DIV:*/
-        elmnt.onmousedown = dragMouseDown;
-      }
+
+      elmnt.onmousedown = dragMouseDown;
 
       function dragMouseDown(e) {
         e = e || window.event;
-        // get the mouse cursor position at startup:
         pos3 = e.clientX;
         pos4 = e.clientY;
         document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
         document.onmousemove = elementDrag;
       }
 
       function elementDrag(e) {
         e = e || window.event;
-        // calculate the new cursor position:
         pos1 = pos3 - e.clientX;
         pos2 = pos4 - e.clientY;
         pos3 = e.clientX;
@@ -30935,7 +30927,6 @@ var BlogPostCreationForm = function (_React$Component) {
       }
 
       function closeDragElement() {
-        /* stop moving when mouse button is released:*/
         document.onmouseup = null;
         document.onmousemove = null;
       }
