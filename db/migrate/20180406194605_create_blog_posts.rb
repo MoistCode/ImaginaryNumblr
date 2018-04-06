b9a95904d664e1314b2bffbcf7a2627faa67bbb8
Tommy Pham <phamtommylong1991@gmail.com>
2018-04-06 12:50:45 -0700

    migrated blogpost table

--- db/migrate/20180406194605_create_blog_posts.rb
+++ db/migrate/20180406194605_create_blog_posts.rb
@@ -1,7 +1,11 @@
 class CreateBlogPosts < ActiveRecord::Migration[5.1]
   def change
     create_table :blog_posts do |t|
-
+      t.integer :author_id, null: false
+      t.string :content_type null: false
+      t.string :title
+      t.text :description
+      t.string :quote
       t.timestamps
     end
   end
