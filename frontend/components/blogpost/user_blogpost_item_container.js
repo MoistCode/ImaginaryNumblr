d4bdad819eb6506382dce1e2ec779750063d7e27
Tommy Pham <phamtommylong1991@gmail.com>
2018-04-07 18:53:34 -0700

    working on individual user specific blogpost items for user showpage

---
+++ frontend/components/blogpost/user_blogpost_item_container.js
@@ -0,0 +1,12 @@
+import BlogpostItem from './blogpost_item';
+import { connect } from 'react-redux';
+
+const mapStateToProps = (state, ownProps) => ({
+
+});
+
+const mapDispatchToProps = (dispatch) => ({
+
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(BlogpostItem);
