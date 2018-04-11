0deb35d003b5fc0350ca8fc8cd5189ff6146d975
Tommy Pham <phamtommylong1991@gmail.com>
2018-04-10 17:10:12 -0700

    Made create and destroy for likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,33 @@
 class LikesController < ApplicationController
+
+  def create
+    if !current_user
+      render json: ['Must be logged in to do that.'], status: 422
+    elsif Blogpost.find(params[:like][:liked_blog_id])
+      @like = Like.new(liked_blog_id: params[:like][:liked_blog_id])
+      @like.liker_id = current_user.id
+      if @like.save
+        @user = User.find(current_user.id)
+        render 'api/users/show'
+      else
+        render json: @like.errors.full_messages, status: 422
+      end
+    else
+      render json: ['That blog does not exist.'], status: 422
+    end
+  end
+
+  def destroy
+    @like = Like.find_by(liker_id: current_user.id, liked_blog_id: params[:id])
+    @like.destroy
+    @user = current_user
+    render 'api/users/show'
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:liked_blog_id)
+  end
+
 end
